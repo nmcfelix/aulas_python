f2563b3e5104e05cc4cdbe68732a788d1f3e20e3
Nuno Félix <nunofelix@Nuno-MBP.local>
2021-04-06 13:29:40 +0100

    Pesquisa e conversao de imagens completo

--- Exercicios/image-playground/JPGtoPNGConverter.py
+++ Exercicios/image-playground/JPGtoPNGConverter.py
@@ -3,7 +3,8 @@ import sys
 from PIL import Image, ImageFilter
 
 source_folder = sys.argv[1]
-#destination_folder = sys.argv[2]
+destination_folder = sys.argv[2]
+
 
 # Find all photos inside source directory
 img_list = []
@@ -11,4 +12,14 @@ for img in os.listdir(source_folder):
     if img.endswith(".jpg"):
         img_list.append(os.path.join(source_folder, img))
 
-print(img_list)
+if os.path.exists(destination_folder):
+    pass
+else:
+    os.mkdir(destination_folder)
+
+for i in img_list:
+    original = Image.open(i)
+    filename, file_extension = os.path.splitext(os.path.basename(i))
+    filename += '.png'
+    new_name = os.path.join(destination_folder, filename)
+    original.save(os.path.join(new_name))
